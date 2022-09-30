abac7c196070cba5de7dfa269277590282ee04f9
Henrique <rickresende03@gmail.com>
2022-09-30 03:09:22 -0300

    inicializando servidor http

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log("Hello, world!");
+import express, {Request, Response, NextFunction} from 'express'
+
+
+const app = express()
+
+
+app.get('/status', (req: Request, res: Response, next: NextFunction) => {
+    res.status(200).send({ foo: 'bar' })
+})
+
+
+
+app.listen(3000, () => {
+    console.log("Servidor rodando na porta: 3000");
+
+})
